57a0b77ad1d14d47ef633a62b6eb3e651ca762ec
Vazgen Sedrakyan <VSedrakyan@beeline.am>
2018-01-30 14:15:50 +0400

    update

---
+++ demo-content/ML-4/manifest.php
@@ -0,0 +1,9 @@
+<?php if (!defined('FW')) die('Forbidden');
+/**
+ * @var string $uri Demo directory url
+ */
+
+$manifest = array();
+$manifest['title'] = __('ML Video Demo', 'fw');
+$manifest['screenshot'] = $uri . '/screenshot.png';
+$manifest['preview_link'] = 'http://vazgen.byethost6.com';

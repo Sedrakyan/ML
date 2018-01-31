fd081cf199a7b5c2c2c5616071c5c6d84301deb4
Vazgen Sedrakyan <VSedrakyan@beeline.am>
2018-01-31 15:12:53 +0400

    update

--- demo-content/ML-3/manifest.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('FW')) die('Forbidden');
-/**
- * @var string $uri Demo directory url
- */
-
-$manifest = array();
-$manifest['title'] = __('ML Slider Demo', 'fw');
-$manifest['screenshot'] = $uri . '/screenshot.png';
-$manifest['preview_link'] = 'http://vazgen.byethost6.com';
